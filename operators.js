b55e09519838231da5700c43ce3b685cf298f000
ramanjaneyalu <ramanjaneyaluvenna@gmail.com>
2024-07-12 20:57:01 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,69 @@
+let string1='Anji'
+console.log(string1, typeof string1)
+
+let num=20
+console.log(num, typeof num)
+
+let bool=true
+console.log(bool, typeof bool)
+
+let undef
+console.log(undef, typeof undef)
+
+let a=null
+console.log(a, typeof a)
+
+let i=10
+let j=5
+//addition
+console.log("sum", i+j)
+//substraction
+console.log("sub", i-j)
+//multiplication
+console.log("mul", i*j)
+//division
+console.log("div", i/j)
+//modulus
+console.log("mod", i%j)
+
+let x=20
+let y="20"
+
+console.log("==", x==y)
+
+console.log("===", x===y)
+
+console.log("not equal !=", x!=y)
+
+console.log("not equal !==", x!==y)
+
+console.log("greater than", x>y)
+
+console.log("less than", x<y)
+
+console.log("greater than or equal to", x>=y)
+
+console.log("less than or equal to", x<=y)
+
+
+
+//output
+/*Anji string
+20 number
+true boolean
+undefined undefined
+null object
+sum 15
+sub 5
+mul 50
+div 2
+mod 0
+== true
+=== false
+not equal false
+not equal true
+greater than false
+less than false
+greater than or equal to true
+less than or equal to true*/
+
